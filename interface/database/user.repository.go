85206f4b838f52f7267618c02a77a6664bdb6afd
nagatapote <hiroaki29p@icloud.com>
2022-12-17 12:55:49 +0900

    add: user update

--- interface/database/user.repository.go
+++ interface/database/user.repository.go
@@ -6,6 +6,7 @@ import (
 	"go-gql-sqlboiler-psql/infrastructure/db"
 	"go-gql-sqlboiler-psql/usecase/repository"
 
+	"github.com/friendsofgo/errors"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
@@ -34,4 +35,14 @@ func (r *userRepositoryImpl) Fetch(ctx context.Context, id int64) (*models.User,
 
 func (r *userRepositoryImpl) Create(ctx context.Context, user *models.User) error {
 	return r.DbUtils.Error(user.Insert(ctx, r.DbUtils.GetDao(ctx), boil.Infer()))
+}
+
+func (r *userRepositoryImpl) Update(ctx context.Context, user *models.User, columns []string) error {
+	cnt, err := user.Update(ctx, r.DbUtils.GetDao(ctx), boil.Whitelist(columns...))
+	if err != nil {
+		return r.DbUtils.Error(err)
+	} else if cnt != 1 {
+		return errors.Errorf("update count = %d", cnt)
+	}
+	return r.DbUtils.Error(err)
 }
--- usecase/repository/user.repository.go
+++ usecase/repository/user.repository.go
@@ -9,4 +9,5 @@ type UserRepository interface {
 	FetchAll(context.Context) ([]*models.User, error)
 	Fetch(context.Context, int64) (*models.User, error)
 	Create(context.Context, *models.User) error
+	Update(context.Context, *models.User, []string) error 
 }
